173f192c3a14624e5f4899f03bdbf3eacff5fd2f
harris-chris <chrisharriscjh@gmail.com>
2021-08-28 19:37:16 +0900

    First commit

---
+++ .ccls-cache/@data@vap@leeway/hand.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string.h>
+
+using std::string;
+using std::cout;
+
+class Hand {
+  public:
+    string name;
+    int gunnery;
+    int seamanship;
+    int leadership;
+
+};
---
+++ .ccls-cache/@data@vap@leeway/hand.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+
+using std::string;
+using std::cout;
+
+class Hand {
+  public:
+    string name;
+    int gunnery;
+    int seamanship;
+    int leadership;
+};
